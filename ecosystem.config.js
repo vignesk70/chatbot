c1aede1dbae48a16ba3caa2cc5dff9b64621f318
Vigneswaren Krishnamoorthy <vignes_k@yahoo.com>
2024-12-23 19:37:58 +0800

    Added env variables to ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,11 @@ module.exports = {
         port: '3000',
         exec_mode: 'cluster',
         instances: 'max',
-        script: './.output/server/index.mjs'
+        script: './.output/server/index.mjs',
+        env: {
+          BEDROCK_AGENT_ID: "LP261QVRT1",
+          BEDROCK_AGENT_ALIAS_ID: process.env.BEDROCK_AGENT_ALIAS_ID
+        }
       }
     ]
   }
